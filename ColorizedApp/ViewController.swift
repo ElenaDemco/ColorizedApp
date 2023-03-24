376aee7b87e8c6dbed8b38741eb05754782746a1
Elena <elena.demco@gmail.com>
2023-03-24 23:19:38 +0100

    Created code for sliders action.

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorBoard: UIView!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
+    @IBOutlet var valueRed: UILabel!
+    @IBOutlet var valueGreen: UILabel!
+    @IBOutlet var valueBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorBoard.layer.cornerRadius = 30
     }
-
-
+    
+    @IBAction func sliderRedAction() {
+        let sliderValue = sliderRed.value
+        valueRed.text = String(format: "%.2f", sliderValue)
+        setupColorBoard()
+    }
+    
+    @IBAction func sliderGreenAction() {
+        let sliderValue = sliderGreen.value
+        valueGreen.text = String(format: "%.2f", sliderValue)
+        setupColorBoard()
+    }
+    
+    @IBAction func sliderBlueAction() {
+        let sliderValue = sliderBlue.value
+        valueBlue.text = String(format: "%.2f", sliderValue)
+        setupColorBoard()
+    }
+    
+    private func setupColorBoard() {
+        let red = CGFloat(sliderRed.value)
+        let green = CGFloat(sliderGreen.value)
+        let blue = CGFloat(sliderBlue.value)
+        
+        colorBoard.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1
+        )
+    }
 }
 
