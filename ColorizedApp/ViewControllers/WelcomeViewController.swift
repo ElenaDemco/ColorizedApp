87533ebaba3af321a672d2e47ee3c980060455ba
Elena <elena.demco@gmail.com>
2023-04-11 21:03:03 +0200

    Create file WelcomeVC

---
+++ ColorizedApp/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  ColorizedApp
+//
+//  Created by Yelena Demchenko on 11.04.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
