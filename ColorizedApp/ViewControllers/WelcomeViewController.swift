1741a581caf8565c0e046d6610ee3e74fd1fe460
Elena <elena.demco@gmail.com>
2023-04-12 19:14:29 +0200

    Create code for WelcomeVC

--- ColorizedApp/ViewControllers/WelcomeViewController.swift
+++ ColorizedApp/ViewControllers/WelcomeViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-final class WelcomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+final class WelcomeViewController: UIViewController, ColorViewControllerDelegate {
+    func applyColor(color: UIColor) {
+        view.backgroundColor = color
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navigationVC = segue.destination as? UINavigationController else { return }
+        guard let colorVC = navigationVC.topViewController as? ColorViewController else { return }
+        colorVC.delegate = self
+    }
 }
+
